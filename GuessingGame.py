c185ba20e4d54c81d31072731a1ea3e49ef7772e
atakiltikidane <Atakilti@hotmail.com>
2020-02-04 02:25:11 -0600

    added a few more lines

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,3 +1,7 @@
 import random
 
-randnum = random(1,9)
+randnum = random(1, 20)
+
+print("Welcome to the guessing game where i'm going to guess a random number and you're going to try and guess it.")
+print("Good Luck! :)")
+
