0d4f57719f9b702b4eaf49667f6377d9d5c46e91
atakiltikidane <Atakilti@hotmail.com>
2020-02-04 14:42:13 -0600

    added a few more lines

--- GuessingGame.py
+++ GuessingGame.py
@@ -3,10 +3,18 @@ import random
 # This generates a random number and sets randnum to it
 randnum = random.randint(1, 20)
 
-print("Welcome to the guessing game where i'm going to guess a random number and you're going to try and guess it.")
-print("Good Luck! :)")
+# game introduction message
+print("Welcome to the guessing game were i'm going to generate a random number between 1-20 and you're going to get "
+      "3 tries to try and guess it.")
+print("Good Luck! ^_^")
 
 num = 0
 count = 1
 
+while count < 4 and num != randnum:
+    num = input("Please guess a number between 1-20: ")
+    if num == randnum:
+        print("You guessed correct. You win!")
+    elif num < randnum:
+        if count != 3:
 
